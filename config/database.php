1cce9be2a08e4acf0f46d0901d7ae7524690fddf
Mantis <tangchunlinit@163.com>
2018-09-27 22:53:02 +0800

    配置加载

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tangchunlinit@gmail.com
+ * Date: 2018/9/26
+ * Time: 10:53 PM
+ */
+return [
+    'DSN' => 'mysql:host=localhost;dbname=spider',
+    'USERNAME' => 'root',
+    'PASSWORD' => 'root'
+];
--- config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tangchunlin
- * Date: 2018/9/26
- * Time: 10:53 PM
- */
